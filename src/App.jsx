db083b47438fb131bd62bd107e6ae52b4edd5dbe
Loqmoha <loqmoha@gmail.com>
2025-04-04 15:39:37 +0000

    second4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRoutes from './routes/AppRoutes';
 // Import Layout components if you want a persistent layout wrapper here
